639c0db8f3fab89c79d2755a106273dca1a12c35
Zak <zakozered@mail.ru>
2021-01-15 12:52:49 +0300

    Full done script for resale report

--- venv/insurance_resale.py
+++ venv/insurance_resale.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pprint import pprint
 
 
+
 def report():
     engine = create_engine("mysql+pymysql://ml_user:U5VVYcxx@46.182.24.8:3306/gfk", pool_pre_ping=True)
     conn = engine.connect()
@@ -17,7 +18,13 @@ def report():
     date_2 = input("Введите дату до в формате YYYY-MM-DD: ")
     date_1 = "'" + date_1 + "'"
     date_2 = "'" + date_2 + "'"
-    loan_gen = int(input("Введите поколение займа: 1 для первичников, 2 для вторичников, 0 для всех вместе - "))
+
+    while True:
+        loan_gen = int(input("Введите поколение займа: 1 для первичников, 2 для вторичников, 0 для всех вместе - "))
+        if loan_gen in (0, 1, 2):
+            break
+
+
 
     total_ins_query = "select staff_name ,count(credit_app_id) as total_apps from(select distinctrow ca.id credit_app_id, sa.action, s.id staff_id, s.name staff_name, ca.creation_date, ca.loan_id from credit_application ca join anketa_step anst on ca.id = anst.app_id join staff_action sa on ca.id = sa.app_id left join staff s on sa.staff_id = s.id where ca.creation_date between " + date_1 + " and " + date_2 + " and ca.id in ( select (ca.id) as check_app from credit_application ca join anketa_step anst on ca.id = anst.app_id join staff_action sa on ca.id = sa.app_id left join staff s on ca.staff_id = s.id where ca.creation_date between " + date_1 + " and " + date_2 + " and anst.step = 'waitingReviewInsurance' and sa.action in ('resaleInsurance', 'refusalInsurance', 'nextWithoutInsurance') and ca.cnt_closed_loans >= 0 ) and sa.action in ('resaleInsurance', 'refusalInsurance', 'nextWithoutInsurance') ) as total_ins group by staff_name"
     total_first_loan_query = "select staff_name ,count(credit_app_id) as total_apps from(select distinctrow ca.id credit_app_id, sa.action, s.id staff_id, s.name staff_name, ca.creation_date, ca.loan_id from credit_application ca join anketa_step anst on ca.id = anst.app_id join staff_action sa on ca.id = sa.app_id left join staff s on sa.staff_id = s.id where ca.creation_date between " + date_1 + " and " + date_2 + " and ca.id in ( select (ca.id) as check_app from credit_application ca join anketa_step anst on ca.id = anst.app_id join staff_action sa on ca.id = sa.app_id left join staff s on ca.staff_id = s.id where ca.creation_date between " + date_1 + " and " + date_2 + " and anst.step = 'waitingReviewInsurance' and sa.action in ('resaleInsurance', 'refusalInsurance', 'nextWithoutInsurance') and ca.cnt_closed_loans = 0 ) and sa.action in ('resaleInsurance', 'refusalInsurance', 'nextWithoutInsurance') ) as total_ins group by staff_name"
@@ -49,6 +56,9 @@ def report():
     rep = t_i.merge(r_i)
     rep["resale %"] = round((rep.resale_apps/rep.total_apps)*100, 0)
     pprint(rep)
+    rep.to_excel(date_1 + " - " + date_2 + " ; " + " loan_gen  =" + str(loan_gen) + ".xlsx")
 
 
 report()
+
+
